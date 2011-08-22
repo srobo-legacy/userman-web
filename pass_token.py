b9bfc62da8457f887a3362665b1719c4a13b51ad
Peter Law <PeterJCLaw@gmail.com>
2011-08-22 21:18:30 +0100

    Add a basic pass_token interface implementation.
    This is not intended to be secure! This is just so there's a working
    interface that can be used to work with, a secure implementation
    should drop into this interface.

---
+++ pass_token.py
@@ -0,0 +1,27 @@
+
+import json
+
+def token_encode(data):
+    """
+    Converts a dictionary into an encoded string.
+    @param data (dict): The data to encode.
+    @returns: An encoded token string.
+    """
+    return json.dumps(data)
+
+def token_decode(token):
+    """
+    Converts a dictionary into an encoded string.
+    @param token (token): The token to decode.
+    @returns: A dictionary of the data that was encoded.
+    """
+    return json.loads(token)
+
+if __name__ == '__main__':
+    orig_data = dict(foo="bar", jam=2)
+    print orig_data
+    token = token_encode(orig_data)
+    print token
+    data = token_decode(token)
+    print data
+    assert orig_data == data
